52e1b9bb65710a46f075df320d3eb5b649225466
Muhammad Anas <anas.dev122@gmail.com>
2026-07-14 11:03:49 +0400

    start making authentication  using better-auth

--- index.js
+++ index.js
@@ -1,13 +1,18 @@
 import express from "express";
-import 'dotenv/config'
+import cors from "cors";
+import "dotenv/config";
 
 const app = express();
-const port = 5000;
+
+const port = process.env.PORT || 5000;
+const host = process.env.LOCAL_IP_ADDRESS;
+
+app.use(cors());
 
 app.get("/", (req, res) => {
   res.send("Hello World!");
 });
 
-app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`);
+app.listen(port, host, () => {
+  console.log(`Server running at http://${host}:${port}`);
 });
